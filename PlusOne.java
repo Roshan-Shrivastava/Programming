c5c17a81358a443d8c92d34d7b0d326d32b358e7
Roshan Shrivastava <roshanshrivastava@Roshans-MacBook-Air.local>
2025-06-10 21:50:32 +0530

    Chnages

--- PlusOne.java
+++ PlusOne.java
@@ -1,10 +1,38 @@
+
+/**
+Q-5 Plus One
+
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+		
+		Example 1:
+
+			Input: digits = [1,2,3]
+			Output: [1,2,4]
+			Explanation: The array represents the integer 123.
+			Incrementing by one gives 123 + 1 = 124.
+			Thus, the result should be [1,2,4].
+			
+			
+			Constraints:
+
+				1 <= digits.length <= 100
+				0 <= digits[i] <= 9
+				digits does not contain any leading 0's.
+
+*/
+
+
+
+
 import java.util.Arrays;
 
 public class PlusOne {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(plusOne(new int[]{1, 2, 3})));
         // Output: [1, 2, 4]
-        System.out.println(Arrays.toString(plusOne(new int[]{9, 9, 9})));
+        System.out.println(Arrays.toString(plusOne(new int[]{8,9, 9, 9})));
         // Output: [1, 0, 0, 0]
     }
     public static int[] plusOne(int[] digits) {
@@ -14,7 +42,9 @@ public class PlusOne {
 				digits[i]++;
 				return digits;
 
-			}
+			}else{
+                digits[i] = 0;
+            }
 		}
 		digits=new int[digits.length+1];
 		digits[0]=1;
